bafa7c9017d5469df3981498be47c5cf6b52e92f
Sandra Kuipers <sandra@skuipers.com>
2018-01-27 15:35:18 +0800

    New and interesting ways to break Gibbon

--- gibbon.php
+++ gibbon.php
@@ -17,6 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 $basePath = dirname(__FILE__);
 $basePath = rtrim(str_replace('\\', '/', $basePath), '/');
 
@@ -29,6 +32,9 @@ if (file_exists($basePath.'/config.php') == false || filesize($basePath.'/config
         exit();
     }
 }
+
+// Setup the composer autoloader
+require_once 'vendor/autoload.php';
 
 // Setup the autoloader
 require_once $basePath.'/src/Autoloader.php';
@@ -53,7 +59,7 @@ $version = $gibbon->getVersion();
 
 
 // Require the system-wide functions
-require_once $basePath.'/functions.php';
+// require_once $basePath.'/functions.php';
 
 
 if ($gibbon->isInstalled() == true) {
@@ -65,3 +71,124 @@ if ($gibbon->isInstalled() == true) {
 	// Initialize using the database connection
 	$gibbon->initializeCore($pdo);
 }
+
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'version' => $version,
+        // 'outputBuffering' => false,
+        // 'logger' => [
+        //     'name' => 'slim-app',
+        //     'level' => Monolog\Logger::DEBUG,
+        //     'path' => __DIR__ . '/../logs/app.log',
+        // ],
+    ],
+];
+
+$app = new \Slim\App($config);
+
+$container = $app->getContainer();
+$container['gibbon'] = $gibbon;
+$container['pdo'] = $pdo;
+$container['autoloader'] = $loader;
+
+
+$app->get('/module/{module}[/{actions:.*}]', function (Request $request, Response $response, array $args) {
+
+    $response->getBody()->write('Hello, lovely.');
+    return $response;
+});
+
+$app->get('/account[/{action}]', function (Request $request, Response $response, array $args) {
+
+    $response->getBody()->write('Hello again, lovely.');
+    return $response;
+});
+
+
+$app->get('/[{page}]', function (Request $request, Response $response, array $args) {
+
+    require_once './functions.php';
+
+    $gibbon = $this->gibbon;
+    $connection2 = $this->pdo->getConnection();
+    $loader = $this->autoloader;
+    $pdo = $this->pdo;
+    $guid = $gibbon->guid();
+    $version = $this->get('settings')['version'];
+
+    $params = $request->getQueryParams();
+
+    $destination = isset($params['q'])? '/'.trim($params['q'], '/') : '';
+
+    $gibbon->session->set('address', $destination);
+    $gibbon->session->set('q', $destination);
+    $gibbon->session->set('module', getModuleName($destination));
+    $gibbon->session->set('action', getActionName($destination));
+
+    if (!empty($destination)) {
+        include '.'.$destination;
+    }
+
+    // $response->getBody()->write('You want to go to '.($destination?? 'nowhere') );
+    // $response->getBody()->write($args['page']?? '');
+
+    return $response;
+})->add(function ($request, $response, $next) {
+
+    require_once './functions.php';
+    
+    $gibbon = $this->gibbon;
+    $pdo = $this->pdo;
+    $version = $this->get('settings')['version'];
+
+    print('
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./themes/Default/css/main.css?v='.$version.'" />
+    </head>
+    <body>
+    <div id="wrap"><div id="header"><div id="header-menu">');
+
+    // Display the main menu
+    $mainMenu = new Gibbon\menuMain($gibbon, $pdo);
+    $mainMenu->setMenu();
+    print($mainMenu->getMenu());
+
+    print('</div></div><div id="content-wrap"><div id="content">');
+
+    $response = $next($request, $response);
+
+    print('</div><div id="sidebar">');
+
+    //Invoke and show Module Menu
+    $menuModule = new Gibbon\menuModule($gibbon, $pdo);
+    print($menuModule->getMenu('full'));
+
+    print('</div><br style="clear: both"></div></body></html>');
+
+    return $response;
+});
+
+
+$app->post('/modules/{module}/{page}', function (Request $request, Response $response, array $args) use ($gibbon, $connection2, $pdo) {
+
+    $guid = $gibbon->guid();
+
+    $destination = '/modules/'.$args['module'].'/'.$args['page'];
+
+    chdir('./modules/'.$args['module'].'/');
+
+    if (!empty($destination)) {
+        include './'.$args['page'];
+    }
+
+    return $response->withHeader('Location', $URL);
+});
+
+
+$app->run();
+
+
+exit;
