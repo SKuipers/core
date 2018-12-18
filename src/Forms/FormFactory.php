7e7a7ad66b8553f68f361b862a7fdf37da23e169
Sandra Kuipers <sandra@skuipers.com>
2018-12-18 13:11:26 +0800

    Forms: add a Person input type with user photos on select

--- src/Forms/FormFactory.php
+++ src/Forms/FormFactory.php
@@ -216,6 +216,11 @@ class FormFactory implements FormFactoryInterface
         return new Input\Username($name);
     }
 
+    public function createPerson($name)
+    {
+        return new Input\Person($name);
+    }
+
     /* PRE-DEFINED LAYOUT --------------------------- */
 
     public function createSubheading($content, $tag = 'h4')
