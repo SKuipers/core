80da963a26add44be17e3b021c0b58edc19d50d8
Sandra Kuipers <sandra@skuipers.com>
2019-02-21 11:43:11 +0800

    System: switch Format class to multi-byte friendly title-case function

--- src/Services/Format.php
+++ src/Services/Format.php
@@ -195,6 +195,7 @@ class Format
      */
     public static function relativeTime($dateString, $tooltip = true)
     {
+        if (empty($dateString)) return '';
         if (strlen($dateString) == 10) $dateString .= ' 00:00:00';
         $date = DateTime::createFromFormat('Y-m-d H:i:s', $dateString);
 
