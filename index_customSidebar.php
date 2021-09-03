f13406407e5ec891d2ad6e91f4e38e825c595336
Ray Clark <raynichc@users.noreply.github.com>
2021-09-03 15:30:06 +1000

    Added Health Code Sidebar for Parents

--- index_customSidebar.php
+++ index_customSidebar.php
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 $output = '';
 
-$roleCategory = getRoleCategory($_SESSION[$guid]['gibbonRoleIDPrimary'], $connection2);
+$roleCategory = getRoleCategory($_SESSION[$guid]['gibbonRoleIDCurrent'], $connection2);
 
 if ($roleCategory == 'Staff') {
     $output .= '<h2>' . __('Staff Portal') . '</h2>';
@@ -28,6 +28,18 @@ if ($roleCategory == 'Staff') {
                        class="fullWidth" value="' . __("Today's Announcements ⇒") . '" 
                        onClick="window.open(\''.$_SESSION[$guid]['webLink'].'/portal/\')" 
                        style="height:32px;margin-bottom:15px; background:#C4ECFF; cursor: pointer;">';
+    $output .= '</p>';
+} else if ($roleCategory == 'Parent') {
+    $i18n = $_SESSION[$guid]['i18n']['code'];
+    $header = $i18n == 'zh_CN' || $i18n == 'zh_HK' ? '上傳澳門健康碼（幼稚園至小六）' :  __('Upload Macau Health Code PK-Grade 6');
+    $body = $i18n == 'zh_CN' || $i18n == 'zh_HK' ? '請於孩子進入學校前上傳他們的澳門健康碼截圖：' : __('Please upload your child\'s Macau Health Code screenshots each morning before they enter the building:');
+    $button = $i18n == 'zh_CN' || $i18n == 'zh_HK' ? '上傳澳門健康碼' :  __('Upload Macau Health Code');
+
+	$output .= '<h2>' . $header . '</h2>';
+	$output .= '<p>'. $body .'<br/>';
+    $output .= '<a class="button" href="'.$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/Health Code/hc_upload.php" style="border: 1px solid #222222;background-color: #eeeeee;;color: #444444;font-weight: bold;font-size: 13px ;text-decoration:none;padding: 8px;display:block;text-align:center;">';
+    $output .= $button;
+    $output .= '</a>';
     $output .= '</p>';
 }
 
