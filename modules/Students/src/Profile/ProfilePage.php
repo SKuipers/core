2bad2fd84aef4dc3742001b31a7b5ee178669957
Sandra Kuipers <skuipers@ichk.edu.hk>
2026-02-28 09:58:25 +0800

    Students: add abstract class for profile pages

---
+++ modules/Students/src/Profile/ProfilePage.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Module\Students\Profile;
+
+use Gibbon\Support\Facades\Access;
+use Gibbon\Contracts\Services\Session;
+
+abstract class ProfilePage
+{
+    protected Session $session;
+
+    protected string $gibbonSchoolYearID;
+    protected string $gibbonPersonID;
+
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    public function setStudent(string $gibbonSchoolYearID, string $gibbonPersonID): self
+    {
+        $this->gibbonSchoolYearID = $gibbonSchoolYearID;
+        $this->gibbonPersonID = $gibbonPersonID;
+
+        return $this;
+    }
+
+    public abstract function checkAccess(): bool;
+
+    public abstract function getOutput(): string;
+}
